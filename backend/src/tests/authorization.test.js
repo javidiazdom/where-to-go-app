495ec34a08fd3c5e3d0628c79a253a1eb3067d14
Javier Díaz Domínguez <javidiazdom@gmail.com>
2021-04-15 13:00:02 +0100

    :wrench: Fixed gitignore isues

--- backend/src/tests/authorization.test.js
+++ backend/src/tests/authorization.test.js
@@ -31,3 +31,31 @@ it('fails when registering existing user', (done) => {
       done();
     });
 });
+
+it('logs in users and returns jwt token', (done) => {
+  request
+    .post('/graphql')
+    .send({
+      query:
+        'mutation { login (email: "test1@gmail.com", password: "1234") { token } }',
+    })
+    .expect(200)
+    .end((err, res) => {
+      if (err) return done(err);
+      done();
+    });
+});
+
+it('throws error when any of the login credentials are wrong', (done) => {
+  request
+    .post('/graphql')
+    .send({
+      query:
+        'mutation { login (email: "test1@gmail.com", password: "1256") { token } }',
+    })
+    .expect(200)
+    .end((err, res) => {
+      expect(res.body.errors[0].message).to.equal('Wrong password');
+      done();
+    });
+});
